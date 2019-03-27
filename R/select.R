80254509cdc4fc0e4420c8c69900567ba23ca608
Guillaume Rivollier <rivollier.g@gmail.com>
2019-03-27 15:48:30 +0100

    init

---
+++ R/select.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param df 
+#' @param ind 
+#'
+#' @return
+#' @export
+#' @import dplyr
+#'
+#' @examples
+select2 <- function(df, ind){
+  if(!is.data.frame(df)) stop("df doit être un data frame")
+}
---
+++ R/test.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' test()
+
+test <- function() {
+  print("Hello World")
+}
